58b1ea37f8735ba907795f4db3bfd9bdd26c8aa2
Nanadoumadji <nanadoumadjiyessain@gmail.com>
2025-05-30 10:28:44 +0000

    modele

---
+++ app/Models/Etudiant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nom', 'prenom', 'date_naissance'];
+
+    public function notes() {
+        return $this->hasMany(Note::class);
+    }
+}
+
---
+++ app/Models/Evaluation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['titre', 'type', 'date'];
+
+    public function notes() {
+        return $this->hasMany(Note::class);
+    }
+}
+
